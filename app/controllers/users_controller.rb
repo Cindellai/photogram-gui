536490c291fb2efb3658d132caa3b72e53afedae
cindellai <cindellai26@gmail.com>
2024-06-18 16:38:52 -0500

    user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,15 @@ class UsersController < ActionController::Base
     @list_of_users = mathing_users.order({ :username => :asc })
 
     render({ :template => "user_templates/index"})
+  end
+
+  def show
+    url_username = params.fetch("path_username")
+
+    mathing_usernames = User.where({ :username => url_username })
+
+    @the_user = mathing_usernames.first
+
+    render({ :template => "user_templates/show"})
 end
 end 
